4ce1d1d6281f12a67b01df3b01401a3a98e3256b
aboodathamneh <abdalrhman.slameh@gmail.com>
2018-10-22 14:40:45 +0300

    init

---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :iOS, '10.0'
+
+target 'lawyers' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for lawyers
+pod 'Firebase/Core'
+
+pod 'Firebase/Database'
+
+pod 'Firebase/Auth'
+
+pod 'Firebase/Storage', '~> 5.0'
+
+pod 'FBSDKCoreKit'  
+
+pod 'FBSDKLoginKit'
+
+pod 'FBSDKShareKit'
+
+pod 'FBSDKPlacesKit'
+
+pod 'FBSDKMessengerShareKit'
+
+end
--- lawyers/ViewController.swift
+++ lawyers/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import FBSDKLoginKit
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+   
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+let LogInButton = FBSDKLoginButton()
+        view.addSubview(LogInButton)
+        LogInButton.frame = CGRect(x: 250, y: 250, width: 250, height: 250)
+    LogInButton.delegate = self
     }
 
-
+func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+    if error != nil{
+        print(error)
+        return
+    }
+    print("succesful")
 }
 
+func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+    print("you loged out facebook ")
+    FBSDKGraphRequest(graphPath: "/me", parameters: ["fields":"id","name":"email"])?.start(completionHandler: { (connection, result, err) in
+        print(123)
+    })
+}
+}
